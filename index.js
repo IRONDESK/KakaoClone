0fca860e47971e905e088076e22805b8b19a218d
SOOCHUL SON <zerozerobase@gmail.com>
2021-12-15 00:20:33 +0900

    feat: #2 footer 내 드롭다운 메뉴 구현

--- index.js
+++ index.js
@@ -6,4 +6,24 @@ function getDate () {
     dateText.innerText = `${today.getMonth() + 1}월 ${today.getDate()}일 ${dayArray[today.getDay()]}요일 소식입니다`;
 }
 
+function addClassOn () {
+    // .footer-line-Left 메뉴
+    const footerLine = document.querySelector('.footer-line')
+    footerLine.addEventListener("click", function (e) {
+        if (e.target.closest("li").classList.contains("top-title")) {
+            e.target.closest("li").classList.toggle("on");
+            e.target.nextElementSibling.classList.toggle("on");
+        }
+    })
+
+    // .footer-line-Right 내 관련 사이트 버튼
+    document.querySelector('.footer-line-Right').addEventListener("click", function (e) {
+        e.target.nextElementSibling.classList.toggle("on");
+        e.target.firstElementChild.classList.toggle("on")
+    })
+}
+
+
+
 getDate();
+addClassOn();
