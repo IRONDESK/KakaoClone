ef7e1b9c05208cd4b20c8824d93d0d3a4425efc0
SOOCHUL SON <zerozerobase@gmail.com>
2021-12-19 16:39:04 +0900

    refactory: #1 달력 아이콘 날짜 자동 수정

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 
 function getDate () {
     const dateText = document.querySelector(".date-text");
+    const dayText1 = document.querySelector(".today-sticky .today-icon-text");
+    const dayText2 = document.querySelector(".today-container .today-icon-text");
     let today = new Date();
     const dayArray = ["일", "월", "화", "수", "목", "금", "토"]
     dateText.innerText = `${today.getMonth() + 1}월 ${today.getDate()}일 ${dayArray[today.getDay()]}요일 소식입니다`;
+    dayText1.innerText = `${today.getDate()}`;
+    dayText2.innerText = `${today.getDate()}`;
 }
 
 function addClassOn () {
